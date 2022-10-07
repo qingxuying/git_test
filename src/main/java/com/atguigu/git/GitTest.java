cdd69d9dd9ee27a63a656f41e80b09be59a2ed83
xpl <473756821@qq.com>
2022-10-07 08:14:09 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author xplstart
+ * @create 2022-10-06 21:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
